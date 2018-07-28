569a1853b93639ebf8949bda1eeaabed46e42e7a
Tiasa Kim <tiasa.kim@gmail.com>
2018-07-28 15:41:42 -0700

    Day 2_07.28

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 from google.appengine.ext import ndb
+import webapp2
+import jinja2
 import os
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+class Main(webapp2.RequestHandler):
+    def get(self):
+        login_template = JINJA_ENVIRONMENT.get_template('templates/template.html')
+        self.response.write(login_template.render())
+
+class UserProfile(webapp2.RequestHandler):
--- model.py
+++ model.py
@@ -2,8 +2,8 @@ from google.appengine.ext import ndb
 
 class User(ndb.Model):
     name = ndb.StringProperty(required=True)
-    age = ndb.IntegerProperty(required=True)
+    password = ndb.StringProperty(required=True)
     email = ndb.StringProperty(required=True)
+    age = ndb.IntegerProperty(required=True)
     college = ndb.StringProperty(required=True)
     courses = ndb.StringProperty(repeated=True) #list of courses/subjects and (un)declared major
-    

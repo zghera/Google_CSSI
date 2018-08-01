b8366994d806d0099c88753f0d1ffec3f786e84b
Zach Ghera <zpghera00@gmail.com>
2018-07-31 22:01:28 -0700

    email WORKS

--- templates/experiment/send_mail.py
+++ templates/experiment/send_mail.py
@@ -1,7 +1,15 @@
-mail.send_mail(sender=sender_address,
-                   to="Albert Johnson <Albert.Johnson@example.com>",
-                   subject="Your account has been approved",
-                   body="""Dear Albert:
+location = "the place"
+user = "Zach"
+emailTo = "zpghera00@gmail.com"
+emailBody =
+emailHtml =
+
+message = mail.EmailMessage(
+        sender="college.connect.cssi@gmail.com",
+        subject="Your account has been approved")
+
+message.to = "Albert Johnson <Albert.Johnson@example.com>"
+message.body = """Dear Albert:
 
 Your example.com account has been approved.  You can now visit
 http://www.example.com/ and sign in using your Google Account to
@@ -10,4 +18,6 @@ access new features.
 Please let us know if you have any questions.
 
 The example.com Team
-""")
+"""
+
+    message.send()

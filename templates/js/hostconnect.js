7faef1d17149217c26e0f4668c560b463fdb2c38
Najib Abdi <abdinajka@gmail.com>
2018-08-02 12:09:37 -0700

    F
    F

--- templates/js/hostconnect.js
+++ templates/js/hostconnect.js
@@ -1,6 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
-    var instances = M.Dropdown.init(elems, options);
+    var instances = M.Dropdown.init(elems, null);
   });
 
-  $('.dropdown-button').dropdown();
+  //$('.dropdown-button').dropdown();
+
+  function myFunction() {
+      document.getElementById("mySelectCourse").classList.toggle("show");
+  }
+
+  function filterFunction() {
+      var input, filter, ul, li, a, i;
+      input = document.getElementById("myInput");
+      filter = input.value.toUpperCase();
+      div = document.getElementById("mySelectCourse");
+      a = div.getElementsByTagName("a");
+      for (i = 0; i < a.length; i++) {
+          if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+              a[i].style.display = "";
+          } else {
+              a[i].style.display = "none";
+          }
+      }
+  }
